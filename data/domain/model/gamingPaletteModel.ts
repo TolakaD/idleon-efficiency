730899a37f9ca12bc7e906b8abeb824e1b7157a4
Donchenko Anatolii <tolakad1@gmail.com>
2026-06-10 13:19:19 +0300

    [Work] Add support for the gaming palette (without luck calculation) (#323)

---
+++ data/domain/model/gamingPaletteModel.ts
@@ -0,0 +1,15 @@
+
+
+export interface GamingPaletteModel {
+    red: number,
+    green: number,
+    blue: number,
+    name: string,
+    bonusValue: number,
+    usesDecay: number,
+    levelUpBase: number,
+    levelUpExponent: number,
+    unlockChanceBase: number,
+    x9: number,
+    bonus: string
+}
